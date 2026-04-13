1df77e8b6af7fc6db757a4c7f4bcbf5bf87f0db9
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:51:51 +0300

    files

---
+++ Favorite-Quote-Practice.js
@@ -0,0 +1,17 @@
+// create a quote
+let quote = "Practice makes perfect";
+
+// print the quote
+console.log(quote);
+
+// print its length
+console.log(quote.length);
+
+// print in uppercase
+console.log(quote.toUpperCase());
+
+// print in lowercase
+console.log(quote.toLowerCase());
+
+// check if it includes a word
+console.log(quote.includes("perfect"));
