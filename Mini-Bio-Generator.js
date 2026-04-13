7ebcf8daf3d3a5eb6d066db863151acea8b83a8b
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:32:02 +0300

    files

---
+++ Mini-Bio-Generator.js
@@ -0,0 +1,32 @@
+//  Basic variables
+let name = "Tony";
+let age = 25;
+let city = "Nairobi";
+let favoriteFood = "Pizza";
+let hobby = "Coding";
+
+// Array of 3 favorite things
+let favorites = ["Music", "Travel", "Football"];
+
+// Object with details
+let person = {
+    name: name,
+    age: age,
+    city: city,
+    hobby: hobby
+};
+
+// Mini bio
+console.log("My name is " + name + ", I am " + age + " years old, I live in " + city + ". I love " + favoriteFood + " and enjoy " + hobby + ".");
+
+// Print array
+console.log("Favorites:", favorites);
+
+// Print object
+console.log("Person:", person);
+
+// Length of name
+console.log("Name length:", name.length);
+
+// First letter of city
+console.log("First letter of city:", city[0]);
