a3915dd00c922758c875452e69387aa880e3c308
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:35:27 +0300

    files

---
+++ Full-Name-Builder.js
@@ -0,0 +1,18 @@
+// Create variables
+let firstName = "Tony";
+let middleName = "Gitonga";
+let lastName = "Kainga";
+
+// Print each one separately
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+
+// Join into full name
+let fullName = (`${firstName} ${middleName} ${lastName}`)
+
+// Print full name
+console.log(fullName);
+
+// Print length of full name
+console.log(fullName.length);
