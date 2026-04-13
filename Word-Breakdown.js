d6ef7b97e5f93808c7b85e04b764df9fc9b5aa8d
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:45:39 +0300

    files

---
+++ Word-Breakdown.js
@@ -0,0 +1,20 @@
+// create a word
+let word = "elephant";
+
+// print the whole word
+console.log(word);
+
+// first character
+console.log(word[0]);
+
+// second character
+console.log(word[1]);
+
+// third character
+console.log(word[2]);
+
+// last character
+console.log(word[word.length - 1]);
+
+// total length
+console.log(word.length);
