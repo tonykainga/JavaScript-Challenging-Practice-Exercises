d109630e48be05ad94750b582027438d3892ad9a
Tony Kainga <tonykainga@gmail.com>
2026-04-13 21:52:04 +0300

    files

---
+++ Reassignment-Investigation.js
@@ -0,0 +1,19 @@
+// assigning different variables
+var studentName = "sarabi";
+let age=20;
+const school= "banda";
+
+// printing the variables 
+
+console.log(studentName);
+console.log(age);
+console.log(school);
+
+// changing variables 
+
+studentName = "Chichia";
+age=25;
+
+console.log(studentName);
+console.log(age);
+console.log(school);
