67cf02aa5fb760905599ed7c9fc00363849fd95b
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:27:42 +0300

    files

---
+++ School-Information-Program.js
@@ -0,0 +1,30 @@
+// School name (string)
+let schoolName = "Moringa School";
+
+// Year (number)
+let year = 2026;
+
+// Boolean (is the school open?)
+let isClosed = false;
+
+// Array of subjects
+let subjects = ["JavaScript", "HTML", "CSS", "Git"];
+
+// Object for one teacher
+let teacher = {
+    name: "Nancy",
+    subject: "JavaScript",
+    experience: 5
+};
+
+// Print everything clearly
+console.log("School Name:", schoolName);
+console.log("Year:", year);
+console.log("Is School closed:", isClosed);
+
+console.log("Subjects:", subjects);
+
+console.log("Teacher Details:");
+console.log("Name:", teacher.name);
+console.log("Subject:", teacher.subject);
+console.log("Experience:", teacher.experience);
