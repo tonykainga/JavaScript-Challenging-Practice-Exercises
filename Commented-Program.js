36d88e5184c48e574b381f89f0e70519f42a9596
Tony Kainga <tonykainga@gmail.com>
2026-04-13 14:07:03 +0300

    files

---
+++ Commented-Program.js
@@ -0,0 +1,8 @@
+var name = "Tony";
+// stores variable name Tony
+var age =35;
+// stores variable age 35
+console.log(name)
+// prints the name tony in the terminal
+console.log(age)
+// prints the age in the terminal
