28c2f319e9c3e85efd6c185b26546d78c4c80f35
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:57:31 +0300

    files

---
+++ Array-of-Dreams.js
@@ -0,0 +1,17 @@
+// create an array of dream countries
+let dreamCountries = ["Thailand", "Jamaica", "Italy", "Brazil", "Indoneasia"];
+
+// print the whole array
+console.log(dreamCountries);
+
+// print the first country
+console.log(dreamCountries[0]);
+
+// print the last country
+console.log(dreamCountries[dreamCountries.length - 1]);
+
+// print the length of the array
+console.log(dreamCountries.length);
+
+// this array is non-primitive because it stores multiple values in one variable
+// and it is an object (arrays are a type of object in JavaScript)
