61afed0146313b1c279c184290b4e2b0f497602a
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:02:58 +0300

    files

---
+++ Student-Object-Practice.js
@@ -0,0 +1,19 @@
+// create a student object
+let student = {
+    name: "Tony",
+    age: 25,
+    country: "Kenya",
+    course: "Software Development"
+};
+
+// print the whole object
+console.log(student);
+
+// print each property separately
+console.log(student.name);
+console.log(student.age);
+console.log(student.country);
+console.log(student.course);
+
+// this object is non-primitive because it stores multiple values
+// and it is a reference type (objects are stored by reference)
