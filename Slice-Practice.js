08b43e8a7506b8669fc5dad1d2334237e53d7d7e
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:12:41 +0300

    files

---
+++ Slice-Practice.js
@@ -0,0 +1,11 @@
+// create variable
+let language = "JavaScript";
+
+// first 4 letters
+console.log(language.slice(0, 4));
+
+// last 6 letters
+console.log(language.slice(-6));
+
+// middle part (you choose)
+console.log(language.slice(4, 10));
