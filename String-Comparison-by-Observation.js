009bebb4036eace56c7e7309cdb4cd2c98da69ad
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:20:50 +0300

    files

---
+++ String-Comparison-by-Observation.js
@@ -0,0 +1,22 @@
+// number vs string
+let num = 30;
+let strNum = "30";
+
+console.log(typeof num);     // number
+console.log(typeof strNum);  // string
+
+// explanation
+// 30 is a number (used for calculations)
+// "30" is a string (text, even though it looks like a number)
+
+
+// boolean vs string
+let bool = true;
+let strBool = "true";
+
+console.log(typeof bool);     // boolean
+console.log(typeof strBool);  // string
+
+// explanation
+// true is a boolean (true/false value)
+// "true" is a string (just text, not a real boolean)
