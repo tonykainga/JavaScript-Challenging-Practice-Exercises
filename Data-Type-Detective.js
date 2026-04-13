495b9983303543dbeeda64f85ab4deec4f43c715
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:14:10 +0300

    files

---
+++ Data-Type-Detective.js
@@ -0,0 +1,15 @@
+
+// assigning different variables
+let school = "Moringa School";
+let x = 2026;
+let studentName = false;
+let studentAge = null;
+let y;
+
+// checking the datatypes in the Variables
+
+console.log(typeof school);
+console.log(typeof 2026);
+console.log(typeof studentName);
+console.log(typeof studentAge);
+console.log(typeof y);
