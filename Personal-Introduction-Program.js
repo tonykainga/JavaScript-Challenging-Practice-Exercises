3d11c18ee61fb0591e5ba56dd4e9ecaeef4696f3
Tony Kainga <tonykainga@gmail.com>
2026-04-13 13:54:51 +0300

    files

---
+++ Personal-Introduction-Program.js
@@ -0,0 +1,11 @@
+let firstName= "Tony";
+let secondName= "Kainga";
+let age= 35;
+let country="Kenya";
+let Food="Sushi";
+let word="JavaScript";
+console.log(`My first name is ${firstName}`)
+console.log(`My second name is ${secondName}`)
+console.log (`I am ${age} years`)
+console.log(`My favourite meal is ${Food}`)
+console.log(`I love learning ${word}`)
