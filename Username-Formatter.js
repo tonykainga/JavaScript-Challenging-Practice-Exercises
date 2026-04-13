51bc6ae22182fe2ec2c29e5f708843be0585ce52
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:41:36 +0300

    files

---
+++ Username-Formatter.js
@@ -0,0 +1,15 @@
+// create a username with spaces
+let username = "   TonyKainga   ";
+
+// print original value
+console.log(username);
+
+// remove spaces
+let cleanName = username.trim();
+console.log(cleanName);
+
+// make it uppercase
+console.log(cleanName.toUpperCase());
+
+// make it lowercase
+console.log(cleanName.toLowerCase());
