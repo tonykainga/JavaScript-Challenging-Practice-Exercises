cb754614204d64bc6dabfb76a730c9176f331d71
Tony Kainga <tonykainga@gmail.com>
2026-04-13 22:26:30 +0300

    files

---
+++ Primitive-vs-Non-Primitive-Classification.js
@@ -0,0 +1,25 @@
+// Primitive variables
+let name = "Tony";          // primitive (string)
+let age = 25;               // primitive (number)
+let isStudent = true;       // primitive (boolean)
+let x;                      // primitive (undefined)
+let y = null;               // primitive (null)
+
+// Non-primitive variables
+let numbers = [1, 2, 3];    // non-primitive (array)
+let person = {              // non-primitive (object)
+    name: "Sarabi",
+    score: 90
+};
+let colors = ["red", "blue", "green"]; // non-primitive (array)
+
+// 🔹 Printing all variables
+console.log(name);     // primitive
+console.log(age);      // primitive
+console.log(isStudent);// primitive
+console.log(x);        // primitive
+console.log(y);        // primitive
+
+console.log(numbers);  // non-primitive
+console.log(person);   // non-primitive
+console.log(colors);   // non-primitive
