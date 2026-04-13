5beaac00c32c199ecca9cbc44ab8a166a3c8fcbd
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:10:08 +0300

    files

---
+++ String-Search-Practice.js
@@ -0,0 +1,8 @@
+// create a sentence
+let sentence = "JavaScript is fun and powerful for building applications";
+
+// check for a word that exists
+console.log(sentence.includes("fun"));
+
+// check for a word that does not exist
+console.log(sentence.includes("soda"));
