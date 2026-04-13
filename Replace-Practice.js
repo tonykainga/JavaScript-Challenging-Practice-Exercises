11fb341a10e76720e59d8546b1b0fbace1ed0f26
Tony Kainga <tonykainga@gmail.com>
2026-04-13 23:18:48 +0300

    files

---
+++ Replace-Practice.js
@@ -0,0 +1,11 @@
+// create a sentence
+let sentence = "I love music";
+
+// print original sentence
+console.log(sentence);
+
+// replace a word
+let newSentence = sentence.replace("music", "rugby");
+
+// print updated sentence
+console.log(newSentence);
